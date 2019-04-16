aa6c041aec62d2117f0a9efdec9c5477291bcc6c
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-04-16 02:34:54 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -5,5 +6,6 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
 
 # code your input_to_index and move method here!
